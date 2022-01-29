587e88edf12c48f37a9c732e518675f7aa090513
KOGUYU <98623415+KOGUYU@users.noreply.github.com>
2022-01-30 03:23:42 +0900

    エンディングインポート2

---
+++ UnityProject/SorgeProject/Assets/ObjectActive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectActive : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+
+
+    void Start()
+    {
+        Invoke("ResutLogo", 1f);
+
+    }
+
+    public void ResutLogo()
+    {
+        target.SetActive(true);
+    }
+
+}
