869c4e9d36183a6b38d35894c09facf26ac7c5de
studioai <studioai@hanmail.net>
2022-01-29 23:31:54 +0900

    Lifespan

--- UnityProject/SorgeProject/Assets/Scripts/Behaviours/InfomationBehaviour.cs
+++ UnityProject/SorgeProject/Assets/Scripts/Behaviours/InfomationBehaviour.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using SorgeProject.Util;
-
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
 using SorgeProject.Data;
 
 namespace SorgeProject.Object
 {
     public class InfomationBehaviour : Draggable
     {
+        [SerializeField]
+        Image LifeSpanMeterImage = null;
+        float FadeAwayTime = 1.0f;
+        CanvasGroup InformationCanvasGroup;
+
         public float LifeTime { get; private set; }
         public int Cost { get; private set; }
 
@@ -23,6 +30,75 @@ namespace SorgeProject.Object
 
             //必ず直すこと
             Cost = 10;
+        }
+
+        private void Awake()
+        {
+            if (LifeSpanMeterImage == null)
+            {
+                EditorUtility.DisplayDialog("Null Error", "Set Meter UI variables", "OK");
+            }
+
+            InformationCanvasGroup = GetComponent<CanvasGroup>();
+
+            if (InformationCanvasGroup == null)
+            {
+                EditorUtility.DisplayDialog("Null Error", "Canvas Group not found", "OK");
+            }
+        }
+
+        private void Start()
+        {
+            if (LifeSpanMeterImage)
+            {
+                LifeSpanMeterImage.fillAmount = 1f;
+                StartCoroutine(LifeSpan(LifeSpanMeterImage, LifeTime));
+            }
+        }
+
+        public IEnumerator LifeSpan(Image fillImage, float lifeTime)
+        {
+            if (fillImage == null)
+                yield break;
+
+            float startTime = Time.time;
+            float time = lifeTime;
+            float value = 0;
+
+            while (Time.time - startTime < lifeTime)
+            {
+                time -= Time.deltaTime;
+                value = time / lifeTime;
+                fillImage.fillAmount = value;
+                yield return null;
+            }
+
+            if (InformationCanvasGroup)
+            {
+                InformationCanvasGroup.interactable = false;
+                InformationCanvasGroup.blocksRaycasts = false;
+                StartCoroutine(FadeAwayAndDestroy(InformationCanvasGroup, FadeAwayTime));
+            }
+        }
+
+        public IEnumerator FadeAwayAndDestroy(CanvasGroup canvasGroup, float lifeTime)
+        {
+            if (canvasGroup == null)
+                yield break;
+
+            float startTime = Time.time;
+            float time = lifeTime;
+            float value = 0;
+
+            while (Time.time - startTime < lifeTime)
+            {
+                time -= Time.deltaTime;
+                value = time / lifeTime;
+                canvasGroup.alpha = value;
+                yield return null;
+            }
+
+            Destroy(this);
         }
     }
 }
