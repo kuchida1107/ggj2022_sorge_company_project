13e9080ac41fa044bc91f82a169e51f4ead56c83
KobayashiTatsuyoshi <91362006+KobayashiTatsuyoshi@users.noreply.github.com>
2022-01-29 20:09:03 +0900

    Revert "Develop"

--- UnityProject/SorgeProject/Assets/Scripts/Behaviours/MoneyBehaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoneyBehaviour : MonoBehaviour
-{
-    [SerializeField] StringView m_stringView;
-    public int View { set => m_stringView.Set(value); }
-}
