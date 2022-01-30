169cffe2ca54b9ce87fb5cd52190a46ec04efa18
kobayashi <traitblow@gmail.com>
2022-01-30 13:03:36 +0900

    一時コミット

--- UnityProject/SorgeProject/Assets/Scripts/Behaviours/MoneyBehaviour.cs
+++ UnityProject/SorgeProject/Assets/Scripts/Behaviours/MoneyBehaviour.cs
@@ -6,10 +6,39 @@ using UnityEngine.UI;
 public class MoneyBehaviour : MonoBehaviour
 {
     [SerializeField] Text m_text;
+    [SerializeField] float m_animTime;
+    int prev = 0;
+    
     public int View { 
         set
         {
-            m_text.text = $"${value:00000}";
+            if (prev != value)
+            {
+                AnimationText(value);
+            }
         }
+    }
+
+    void AnimationText(int value)
+    {
+        StartCoroutine(IntegerAnimation(value));
+    }
+
+    IEnumerator IntegerAnimation(int value)
+    {
+        int _prev = prev;
+        prev = value;
+        float timer = m_animTime;
+        while (timer > 0)
+        {
+            float t = timer / m_animTime;
+
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void SetText(int value)
+    {
+        m_text.text = $"${value:00000}";
     }
 }
